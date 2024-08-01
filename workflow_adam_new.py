3f2ef4f00a6f49c2e7363dd96e876508d14eb0c4
Ross Sharp <ross.sharp@dominodatalab.com>
2024-08-01 12:43:41 -0700

    Pushed from Domino: https://pharma.domino-eval.com/workspace/ross_domino/CDISC01_RE_FLOWSDEV?executionId=66ab8e68ac7e5c068fac61b7

--- workflow_adam_new.py
+++ workflow_adam_new.py
@@ -4,22 +4,34 @@ from typing import TypeVar, NamedTuple
 from flytekitplugins.domino.helpers import Input, Output, run_domino_job_task
 from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, EnvironmentRevisionSpecification, EnvironmentRevisionType, DatasetSnapshot
 
-# pyflyte run --remote workflow_adam_new.py ADaM_NEW --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND/34
-
+# pyflyte run --remote workflow_adam_new.py ADaM_NEW --sdtm_dataset_snapshot /mnt/data/snapshots/SDTMBLIND/1
 
 @workflow
 def ADaM_NEW(sdtm_dataset_snapshot: str): # -> FlyteFile[TypeVar("sas7bdat")]:
 
-    # First task using helper method
+    #Crete ADSL dataset. The only input is the SDTM Dataset. 
     adsl = run_domino_job_task(
         flyte_task_name="Create ADSL Dataset",
         command="prod/adam_flows/ADSL.sas",
         inputs=[Input(name="sdtm_dataset_snapshot", type=str, value=sdtm_dataset_snapshot)],
-        output_specs=[Output(name="ADSL Dataset", type=FlyteFile[TypeVar("sas7bdat")])],
+        output_specs=[Output(name="adsl", type=FlyteFile[TypeVar("sas7bdat")])],
         use_project_defaults_for_omitted=True,
         environment_name="SAS Analytics Pro",
-        dataset_snapshots=[DatasetSnapshot(Id="64e3c80427f0ef13b5d3a73c", Version=34)]
+        dataset_snapshots=[DatasetSnapshot(Id="66a2984f62fa8d3bb129c689", Version=1)]
     ) 
+
+    #Crete ADAE dataset. This has two inputs, the SDTM Dataset and the output from the previous task i.e. ADSL. 
+    adae = run_domino_job_task(
+        flyte_task_name="Create ADAE Dataset",
+        command="prod/adam_flows/ADAE.sas",
+        inputs=[Input(name="sdtm_dataset_snapshot", type=str, value=sdtm_dataset_snapshot),
+                Input(name="adsl", type=FlyteFile[TypeVar("sas7bdat")], value=adsl["adsl"])],
+        output_specs=[Output(name="adae", type=FlyteFile[TypeVar("sas7bdat")])],
+        use_project_defaults_for_omitted=True,
+        environment_name="SAS Analytics Pro",
+        dataset_snapshots=[DatasetSnapshot(Id="66a2984f62fa8d3bb129c689", Version=1)]
+    )
+    
 
     # Output from the task above will be used in the next step
 
