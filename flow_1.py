c46ffe1760518e5e560bb203cc9a9fd3decab44c
Ross Sharp <ross.sharp@dominodatalab.com>
2024-08-02 13:45:12 -0700

    Pushed from Domino: https://pharma.domino-eval.com/workspace/CDISC01_Team/CDISC01_RE_CSR?executionId=66ad446e0dd5b125cc3b88b6

--- flow_1.py
+++ flow_1.py
@@ -4,10 +4,10 @@ from typing import TypeVar, NamedTuple
 from flytekitplugins.domino.helpers import Input, Output, run_domino_job_task
 from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, EnvironmentRevisionSpecification, EnvironmentRevisionType, DatasetSnapshot
 
-# pyflyte run --remote workflow_full.py ADaM_TFL --sdtm_dataset_snapshot /mnt/data/snapshots/SDTMBLIND/1
+# pyflyte run --remote flow_1.py ADaM_only --sdtm_dataset_snapshot /mnt/data/snapshots/SDTMBLIND/1
 
 @workflow
-def ADaM_TFL(sdtm_dataset_snapshot: str): # -> FlyteFile[TypeVar("sas7bdat")]:
+def ADaM_only(sdtm_dataset_snapshot: str): # -> FlyteFile[TypeVar("sas7bdat")]:
 
     #Crete ADSL dataset. The only input is the SDTM Dataset. 
     adsl = run_domino_job_task(
@@ -74,16 +74,6 @@ def ADaM_TFL(sdtm_dataset_snapshot: str): # -> FlyteFile[TypeVar("sas7bdat")]:
         use_project_defaults_for_omitted=True,
         environment_name="SAS Analytics Pro",
         dataset_snapshots=[DatasetSnapshot(Id="66a2984f62fa8d3bb129c689", Version=1)]
-    )
-
-    t_pop = run_domino_job_task(
-        flyte_task_name="Create T_POP Report",
-        command="prod/tfl_flows/t_pop.sas",
-        inputs=[Input(name="adsl", type=FlyteFile[TypeVar("sas7bdat")], value=adsl["adsl"])],
-        output_specs=[Output(name="t_pop", type=FlyteFile[TypeVar("pdf")])],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro",
-        dataset_snapshots=[DatasetSnapshot(Id="66a2984f62fa8d3bb129c689", Version=1)] #Metadata Dataset
     )
 
     # Output from the task above will be used in the next step
