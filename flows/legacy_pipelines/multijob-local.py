c8ec1871bd7d08432bfb23c2ac70ea2eb8ea9a42
Nicholas Jablonski <nick.jablonski@dominodatalab.com>
2024-10-03 11:58:43 -0400

    Update multijob-local.py

--- flows/legacy_pipelines/multijob-local.py
+++ flows/legacy_pipelines/multijob-local.py
@@ -146,9 +146,6 @@ def start_background_job(task, command, out_log_path, err_log_path, add_timestam
     logger.info(f'{task}: Starting command: {command}')
 
     # Launch the command as a background job
-    title = command.split()[0]
-    logger.info('Invoking Domino CLI')
-    command = "/opt/domino/domino run --local --title " + title + command
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     out_thread = LogThread(task, process.stdout, out_log_path, False, add_timestamp)
